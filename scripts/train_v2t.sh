d29eb6d46533c0fd1100763f8e0c76ef9c16401a
huangpeilin.lynn <huangpeilin.lynn@bytedance.com>
2026-06-08 17:08:54 +0800

    update train_v2t.sh

--- scripts/train_v2t.sh
+++ scripts/train_v2t.sh
@@ -6,10 +6,10 @@ PROJECT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
 cd "$PROJECT_DIR"
 
 # NOTE: use absolute paths, modify according to your setting.
-DATA_DIR="${DATA_DIR:-/data2/final_project_data/30h_data}"
-TOKENIZER_PATH="${TOKENIZER_PATH:-/data2/final_project_data/spm1000/spm_unigram1000.model}"
-PRETRAIN_PATH="${PRETRAIN_PATH:-/data2/final_project_ckpt/pretrained_model.pth}"
-RUN_DIR="${RUN_DIR:-$PROJECT_DIR/exp/exp_v2t}"
+DATA_DIR="${DATA_DIR:-/hdd/huangpl/aiintro_final_project/final_project_data/30h_data}"
+TOKENIZER_PATH="${TOKENIZER_PATH:-/hdd/huangpl/aiintro_final_project/final_project_data/spm1000/spm_unigram1000.model}"
+PRETRAIN_PATH="${PRETRAIN_PATH:-/hdd/huangpl/aiintro_final_project/final_project_ckpt/pretrained_model.pth}"
+RUN_DIR="${RUN_DIR:-/hdd/huangpl/aiintro_final_project/run_dir/exp_v2t}"
 
 export PYTHONPATH="$PROJECT_DIR:${PYTHONPATH:-}"
 python -u main.py --config-dir "$PROJECT_DIR/configs/" \
